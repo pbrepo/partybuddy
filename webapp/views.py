43f179556c2de1230311a5525ca937dcf9d852bd
SujithKandamuri <ksnvsujith@gmail.com>
2016-11-28 14:24:43 +0530

    added default view

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import View
+
+
+
+class HomeView(View):
+
+  def get(self, request, *args, **kwargs):
+    return HttpResponse("Hello World")
 
 # Create your views here.
